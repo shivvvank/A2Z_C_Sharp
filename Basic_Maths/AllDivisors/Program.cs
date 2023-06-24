9a8de281fde03880675088df2f2a8557443b61f7
shivvvank <shivvvank@gmail.com>
2023-06-25 03:28:07 +0530

    Added basic maths problems

---
+++ Basic_Maths/AllDivisors/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Example {
+   class Example {
+
+    static List<int> FindDivisors(int n)
+    {
+        List<int> ans = new List<int>();
+        ans.Add(1);
+        if(n!=1)
+            ans.Add(n);
+        for(int i=2;i<=Math.Sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                ans.Add(i);
+                ans.Add(n/i);
+            }
+        }
+        ans.Sort();
+        return ans;
+    }
+    public static void Main(string[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        List<int> l = FindDivisors(n);
+        foreach(int x in l)
+        {
+            System.Console.Write(x+" ");
+        }
+        System.Console.WriteLine();
+    }
+   }
+}
---
+++ Basic_Maths/CheckPrime/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Example {
+   class Example {
+
+    static bool CheckPrime(int n)
+    {
+        for(int i=2;i<=Math.Sqrt(n);i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void Main(string[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        if(CheckPrime(n))
+            System.Console.WriteLine("The number is Prime");
+        else
+            System.Console.WriteLine("The number is not prime");
+    }
+   }
+}
