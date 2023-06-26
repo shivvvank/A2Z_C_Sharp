ac2ddfe95143c4ce9ff8d7342e5172ab80e0d167
shivvvank <shivvvank@gmail.com>
2023-06-26 22:10:17 +0530

    Added Basic Recursion problems

---
+++ Basic_Recursion/FactorialOfN/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example {
+    class Example {
+
+        static int FactorialOfN(int n)
+        {
+            if(n==0)
+                return 1;
+            return n*FactorialOfN(n-1);
+        }
+        public static void Main(string[] args){
+            int n = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Factorial Of "+n+" is "+FactorialOfN(n));
+        }
+    }
+}
---
+++ Basic_Recursion/SumOfNNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Example {
+    class Example {
+
+        static int SumFirstN(int n)
+        {
+            if(n==0)
+                return 0;
+            return n + SumFirstN(n-1);
+        }
+        public static void Main(string[] args){
+            int n = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The sum of first "+n+" numbers is "+SumFirstN(n));
+        }
+    }
+}
