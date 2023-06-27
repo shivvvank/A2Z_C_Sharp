9a0e5aacb2d352880b1c8ae2707266fa9326f386
shivvvank <shivvvank@gmail.com>
2023-06-27 23:11:26 +0530

    Added Arrays Easy  problems

---
+++ Arrays/Easy/LeftRotateBy1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Buffers;
+using System.Numerics;
+
+namespace Example {
+    class Example {
+
+        // 1 2 3 3 3 3 3
+        // 
+        static void LeftRotateBy1(ref int[] a,int n)
+        {  
+            int temp = a[0];
+            for(int i=0;i < n-1;i++)
+            {
+                a[i]=a[i+1];
+            }
+            a[n-1]=temp;
+        }
+        public static void Main(string[] args){
+            Console.WriteLine("Enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            System.Console.WriteLine("Enter Array elements");
+            for(int i=0;i<n;i++)
+            {
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            LeftRotateBy1(ref a, n);
+            System.Console.WriteLine("Array after left rotation.");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ Arrays/Easy/LeftRotateByD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers;
+using System.Numerics;
+
+namespace Example {
+    class Example {
+
+        static void LeftRotateByD(ref int[] a,int n,int d)
+        {  
+            d = d%n;
+            int[] temp = new int[d];
+            for(int i=0;i<d;i++)
+            {
+                temp[i]=a[i];
+            }
+            for(int i=0;i<n-d;i++)
+            {
+                a[i]=a[i+d];
+            }
+            for(int i=n-d;i<n;i++)
+            {
+                a[i]=temp[i-n+d];
+            }
+        }
+        public static void Main(string[] args){
+            Console.WriteLine("Enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            System.Console.WriteLine("Enter Array elements");
+            for(int i=0;i<n;i++)
+            {
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            System.Console.WriteLine("Enter the places to left rotate");
+            int d = Convert.ToInt32(Console.ReadLine());
+            LeftRotateByD(ref a,n,d);
+            System.Console.WriteLine("Array after left rotation by "+d+" places.");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
