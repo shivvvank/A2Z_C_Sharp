a025cc556b897cb10435edcf08b421663c2f9681
shivvvank <shivvvank@gmail.com>
2023-06-27 19:08:44 +0530

    Added Sorting II problems

---
+++ Sorting_II/MergeSort/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Buffers;
+using System.Numerics;
+
+namespace Example {
+    class Example {
+
+        static void MergeSort(ref int[] a,int n)
+        {
+            MergeSorthelper(ref a,0,n-1);
+        }
+        static void Merge(ref int[] a,int low,int mid,int high)
+        {
+            int[] temp = new int[high-low+1];
+            int i = low,j=mid+1,k=0;
+            while(i<=mid&&j<=high)
+            {
+                if(a[i]<=a[j])
+                {
+                    temp[k++]=a[i++];
+                }
+                else
+                {
+                    temp[k++]=a[j++];
+                }
+            }
+            while(i<=mid)
+            {
+                temp[k++]=a[i++];
+            }
+            while(j<=high)
+            {
+                temp[k++]=a[j++];
+            }
+            i=0;
+            for(int b = low;b<=high;b++)
+            {
+                a[b]=temp[i++];
+            }
+        }
+        static void MergeSorthelper(ref int[] a,int low,int high)
+        {
+            if(low==high)
+            {
+                return;
+            }
+            int mid = low+(high-low)/2;
+            MergeSorthelper(ref a,low,mid);
+            MergeSorthelper(ref a,mid+1,high);
+            Merge(ref a,low,mid,high);
+        }
+        static void swap(ref int a,ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        public static void Main(string[] args){
+            Console.WriteLine("Enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Enter Array elements");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            System.Console.WriteLine("Array without Sorting : ");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+            MergeSort(ref a,n);
+            System.Console.WriteLine("Array with Sorting");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ Sorting_II/QuickSort/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Buffers;
+using System.Numerics;
+using System.Threading.Tasks.Sources;
+
+namespace Example {
+    class Example {
+
+        static void QuickSort(ref int[] a,int n)
+        {
+            QuickSorthelper(ref a,0,n-1);
+        }
+        static int Partition(ref int[] a,int low,int high)
+        {
+            int pivot = low;
+            int i = low,j=high;
+            while(i<j)
+            {
+                while(a[i]<=a[pivot]&&i<=high-1)
+                {
+                    i++;
+                }
+                while(a[j]>a[pivot]&&j>=low+1)
+                {
+                    j--;
+                }
+                if(i<j)
+                {
+                    swap(ref a[i],ref a[j]);
+                }
+            }
+            swap(ref a[j],ref a[pivot]);
+            return pivot;
+        }
+        static void QuickSorthelper(ref int[] a,int low,int high)
+        {
+            if(low>=high)
+                return;
+            int pivot = Partition(ref a,low,high);
+            QuickSorthelper(ref a,low,pivot-1);
+            QuickSorthelper(ref a,pivot+1,high);
+        }
+        static void swap(ref int a,ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        public static void Main(string[] args){
+            Console.WriteLine("Enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Enter Array elements");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            System.Console.WriteLine("Array without Sorting : ");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+            QuickSort(ref a,n);
+            System.Console.WriteLine("Array with Sorting");
+            foreach(int i in a)
+            {
+                System.Console.Write(i+" ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
