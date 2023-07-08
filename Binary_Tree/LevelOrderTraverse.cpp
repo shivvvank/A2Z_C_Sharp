e7fc7bee353e144379f6875b5c3642498572667e
shivvvank <shivvvank@gmail.com>
2023-07-09 03:39:13 +0530

    Added Binary Tree introduction

---
+++ Binary_Tree/LevelOrderTraverse.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public : 
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        this->val=val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+    Node(int val,Node* left,Node* right)
+    {
+        this->val=val;
+        this->left=left;
+        this->right=right;
+    }
+};
+Node* createBT()
+{
+    Node* root = new Node(1);
+    Node* temp1 = new Node(2);
+    Node* temp2 = new Node(3);
+    Node* temp3= new Node(4);
+    Node* temp4 = new Node(5);
+    Node* temp5 = new Node(6);
+    Node* temp6 = new Node(7);
+    Node* temp7 = new Node(8);
+    root->left = temp1;
+    root->right = temp2;
+    temp1->left = temp3;
+    temp1->right = temp4;
+    temp2->left = temp5;
+    temp2->right = temp6;
+    temp3->left = temp7;
+    return root;
+}
+void levelorder(Node* root)
+{
+    if(root==nullptr)
+        return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+        cout<<temp->val<<" ";
+        if(temp->left!=nullptr)
+            q.push(temp->left);
+        if(temp->right!=nullptr)
+            q.push(temp->right);
+    }
+}
+int main(){
+    Node* root = createBT();
+    levelorder(root);
+    cout<<endl;
+    return 0;
+}
+/*
+
+            1
+        2       3
+    4       56      7
+8
+
+levelorder - 1 2 3 4 5 6 7 8 
+*/
---
+++ Binary_Tree/PostorderTraverse.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public : 
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        this->val=val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+    Node(int val,Node* left,Node* right)
+    {
+        this->val=val;
+        this->left=left;
+        this->right=right;
+    }
+};
+Node* createBT()
+{
+    Node* root = new Node(1);
+    Node* temp1 = new Node(2);
+    Node* temp2 = new Node(3);
+    Node* temp3= new Node(4);
+    Node* temp4 = new Node(5);
+    Node* temp5 = new Node(6);
+    Node* temp6 = new Node(7);
+    Node* temp7 = new Node(8);
+    root->left = temp1;
+    root->right = temp2;
+    temp1->left = temp3;
+    temp1->right = temp4;
+    temp2->left = temp5;
+    temp2->right = temp6;
+    temp3->left = temp7;
+    return root;
+}
+void postorder(Node* root)
+{
+    if(root==nullptr)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->val<<" ";
+}
+int main(){
+    Node* root = createBT();
+    postorder(root);
+    cout<<endl;
+    return 0;
+}
+/*
+
+            1
+        2       3
+    4       56      7
+8
+
+postorder - 8 4 5 2 6 7 3 1 
+*/
