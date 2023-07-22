229283bbdec945016f8d30c7ac4588390600f319
shivvvank <shivvvank@gmail.com>
2023-07-23 05:12:06 +0530

    Adding graph and new input txt file

--- Graphs/IntroGraphs.cpp
+++ Graphs/IntroGraphs.cpp
@@ -1,34 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-pair<int,vector<vector<int>>> createGraph()
-{
-    int m,n;
-    vector<vector<int>> edges;
-    cout<<"Enter number of Nodes : "<<endl;
-    cin>>n;
-    cout<<"Enter the number of edges : "<<endl;
-    cin>>m;
-    for(i=0;i<m;i++)
-    {
-        vector<int> temp;
-        int t1,t2;
-        cin>>t1>>t2;
-        temp.push_back(t1);
-        temp.push_back(t2);
-        edges.push_back(temp);
-    }
-    return {n,edges};
-}
 int main()
 {
-    pair<int,vector<vector<int>>> t = createGraph(); 
-    int n = t.first;
-    vector<vector<int>> edges = t.second;
-    vector<int> adj[n+1];
-    for(int i=0;i<edges.size();i++)
+    int n,m;
+    freopen("../input.txt","r",stdin);
+    cin>>n>>m;
+    vector<int> adj[n+1]; //Adjacency list
+    for(int i=1;i<=m;i++)
     {
-        adj[edges[i][0]].push_back(edges[i][1]);
-        adj[edges[i][1]].push_back(edges[i][0]);
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" : ";
+        for(auto j : adj[i])
+        {
+            cout<<j<<", ";
+        }
+        cout<<endl;
     }
     return 0;
 }
